3eee07e7cce325e50cb1d5e0eca40ad8de34b552
matiaspizzino <52511628+matiaspizzino@users.noreply.github.com>
2019-08-03 12:21:01 -0300

    Add files via upload

---
+++ ocho.js
@@ -0,0 +1,59 @@
+function mostrar()
+{
+var letra;
+var numero;
+var respuesta;
+var pares;
+var impar;
+var contador=0;
+var promedio=0;
+var suma;
+var resta;
+var maximo;
+var minimo;
+
+
+respuesta=("S");
+while(respuesta=="S")
+{
+    numero=prompt("ingrese un numero");
+    letra=prompt("ingrese una letra");
+}
+if(numero%2==0)
+{
+   pares++;
+}
+else
+{
+    impar++;
+}
+if(numero==0)
+{
+    cero++;
+}
+
+if(numero==0)
+{
+    suma++;
+}
+else
+{
+    resta++;
+}
+
+if(numero==0)
+{
+    promedio++;
+}
+
+alert=("la letra es minimo");
+alert=("el numero es maximo");
+respuesta=prompt("desea seguir ingresando  S o N");
+
+
+
+
+
+
+}
+
